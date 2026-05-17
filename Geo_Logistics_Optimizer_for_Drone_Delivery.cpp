d91d76ab2dc551be75a5d127a843128b2ad7d287
myfeith226-coder <my.feith226@gmail.com>
2026-05-17 19:53:49 +0700

    coba commit ke github

---
+++ Geo_Logistics_Optimizer_for_Drone_Delivery.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+//Node dasar untuk linked list, queue, dan stack paket
+struct NodePaket {
+int idPaket;
+char namaTujuan[50];
+double bobot;
+NodePaket* next; // Pointer untuk menghubungkan ke node selanjutnya
+};
+
+int main() {
+
+    return 0;
+}
