b15230c38a773ffe78f56a812882819b2d01ede1
myfeith226-coder <my.feith226@gmail.com>
2026-05-18 19:37:44 +0700

    Fix bug queue

--- Geo_Logistics_Optimizer_for_Drone_Delivery.cpp
+++ Geo_Logistics_Optimizer_for_Drone_Delivery.cpp
@@ -5,7 +5,6 @@ using namespace std;
 //Node dasar untuk linked list, queue, dan stack paket
 struct NodePaket {
 int idPaket;
-char namaTujuan[50];
 double bobot;
 NodePaket* next; // Pointer untuk menghubungkan ke node selanjutnya
 };
@@ -31,13 +30,14 @@ class CustomQueue {
 
     // Fungsi untuk cek apakah antrian sudah kosong
     bool isEmpty() {
-        return (Head = nullptr);
+        return (Head == nullptr);
     }
 
     // Menambah elemen ke belakang antrean (enqueue)
-    void enqueue(int idPaket) {
+    void enqueue(int idPaket, double bobot = 0.0) {
         NodePaket* nodebaru = new NodePaket();
         nodebaru->idPaket = idPaket;
+        nodebaru->bobot = bobot;
         nodebaru->next = nullptr;
 
         // JIka antrean masih kosong nodebaru akan menjadi head sekaligus tail
@@ -54,7 +54,7 @@ class CustomQueue {
     // Fungsi Dequeue untuk menghapus elemen dari posisi depan antrean (Head)
     void dequeue() {
         if(isEmpty()) {
-        cout << "Antrean kosong! tidak ada data ID paket yang bisa di hapus" << endl;
+        cout << "Antrean kosong! tidak ada data antrean paket yang bisa di hapus" << endl;
         return;
         }
 
@@ -75,10 +75,51 @@ class CustomQueue {
         cout << datadihapus << " telah keluar dari antrean " << endl;
     }
 
-    
+    // Fungsi untuk menampilkan elemen queue paling depan
+    void peek() {
+        if(isEmpty()) {
+            cout << "Antrean paket Kosong!" << endl;
+        } else {
+            cout << "Elemen paling depan" << Head->idPaket << endl;
+        }
+    }
+
+    // Melihat semua antrian
+    void tampilkanqueue() {
+        if(isEmpty()) {
+            cout << "Antrean paket Kosong!" << endl;
+            return;
+        }
+
+        cout << "Isi antrian paket saat ini: " << endl;
+        NodePaket* saatini = Head;
+        while(saatini != nullptr) {
+            cout << saatini->idPaket << "->";
+            saatini = saatini->next;
+        }
+        cout << "NULL" << endl;
+    }
 };
 
 int main() {
+
+CustomQueue antrianPaket;
+
+cout << "=== UJI COBA QUEUE ==" << endl;
+antrianPaket.enqueue(10);
+antrianPaket.enqueue(20);
+antrianPaket.enqueue(30);
+antrianPaket.enqueue(40);
+antrianPaket.enqueue(50);
+
+antrianPaket.tampilkanqueue();
+antrianPaket.peek();
+
+cout << "=== UJI COBA DEQUEUE ==" << endl;
+antrianPaket.dequeue();
+antrianPaket.dequeue();
+antrianPaket.tampilkanqueue();
+antrianPaket.peek();
 
     return 0;
 }
